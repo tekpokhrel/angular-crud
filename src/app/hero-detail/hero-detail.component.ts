904a891f5ae057eeebc6cbd339d8e538c22a0c8d
Tek Pokhrel <teknath.pokharel86@gmail.com>
2020-07-31 18:54:25 -0500

    Latest angular app with CRUD operation

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+    hero: Hero;
+
+    constructor(
+        private route: ActivatedRoute,
+        private location: Location,
+        private heroService: HeroService
+    ) { }
+
+    ngOnInit(): void {
+        this.getHero();
+    }
+
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        //console.log("id", id);
+        this.heroService.getHero(id)
+        .subscribe(hero =>{
+            this.hero = hero;
+        });
+    }
+
+    save(): void {
+        this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+    
+    goBack(): void {
+        this.location.back();
+    }
+
+}
